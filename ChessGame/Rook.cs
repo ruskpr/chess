bd3c824a388c79c6e72544fddbd1b1856bed900a
Rustu Koprulu <rustulp@gmail.com>
2022-10-20 23:01:22 -0600

    refactored Rook code

--- ChessGame/Rook.cs
+++ ChessGame/Rook.cs
@@ -9,12 +9,11 @@ namespace ChessGame
 {
     public class Rook : Piece
     {
-
-        public Rook(Player player, Tile tile) : base(player, tile)
-        {
+        #region Constructor
+        public Rook(Player player, Tile tile) : base(player, tile) =>
             this.Image = player == Player.Player_One ? MyAssets.W_RookImg : MyAssets.B_RookImg;
-        }
-
+        #endregion
+        #region Public Methods
         public override List<Tile> GetValidMoves(Board board, Tile selTile)
         {
             List<Tile> validMoves = new List<Tile>();
@@ -26,64 +25,51 @@ namespace ChessGame
 
             return validMoves;
         }
-
+        #endregion
         #region Private Methods
+
         #region Forward Movement
         private List<Tile> CastForwardMovement(Board b, Tile t)
         {
-            List<Tile> validMoves = new List<Tile>();
+            List<Tile> validMoves = new List<Tile>(); // list that will be returned
 
-            int currentX = t.CoordinateX;
+            int currentX = t.CoordinateX; // added for readability
             int currentY = t.CoordinateY;
 
             if (t.CurrentPiece.CurrentPlayer == Piece.Player.Player_One && GameManager.Turn == GameManager.PlayerTurn.p1)
             {
                 for (int i = currentY; i >= 0; i--) //cast movement upward
-                {
                     if (t.CoordinateY != i)
                     {
                         if (b.Tiles[i, currentX].CurrentPiece != null) // if there is a piece occupying a tile...
                         {
-                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_One) // if its a player on same team
-                            {
+                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_One) // if its a piece on same team stop casting direction
                                 break;
-                            }
-                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_Two) // if its a player on other team
-                            {
-                                validMoves.Add(b.Tiles[i, currentX]);
-                                break;
-                            }
+                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_Two) // if its a piece on other team cast valid space on it and stop
+                                validMoves.Add(b.Tiles[i, currentX]); break;
                         }
 
-                        validMoves.Add(b.Tiles[i, currentX]);
+                        validMoves.Add(b.Tiles[i, currentX]); // add valid move if space is empty
                     }
-                }
             }
             if (t.CurrentPiece.CurrentPlayer == Piece.Player.Player_Two && GameManager.Turn == GameManager.PlayerTurn.p2)
             {
                 for (int i = currentY; i >= 0; i--) //cast movement upward
-                {
                     if (t.CoordinateY != i)
                     {
                         if (b.Tiles[i, currentX].CurrentPiece != null) // if there is a piece occupying a tile...
                         {
-                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_Two) // if its a player on same team
-                            {
+                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_Two) // if its a piece on same team stop casting direction
                                 break;
-                            }
-                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_One) // if its a player on other team
-                            {
-                                validMoves.Add(b.Tiles[i, currentX]);
-                                break;
-                            }
+                            if (b.Tiles[i, currentX].CurrentPiece.CurrentPlayer == Piece.Player.Player_One) // if its a piece on other team cast valid space on it and stop
+                                validMoves.Add(b.Tiles[i, currentX]); break;
                         }
 
-                        validMoves.Add(b.Tiles[i, currentX]);
+                        validMoves.Add(b.Tiles[i, currentX]); // add valid move if space is empty
                     }
-                }
             }
 
-            return validMoves;
+            return validMoves; // return valid forward spaces
         }
         #endregion
         #region Backward Movement
@@ -261,6 +247,7 @@ namespace ChessGame
             return validMoves;
         }
         #endregion
+
         #endregion
     }
 }
