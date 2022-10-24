a1850bc5e6ca189ff29acf7c53a29175eb970715
ruskpr <rustulp@gmail.com>
2022-10-24 00:56:53 -0600

    king ignores moves where another piece is sitting

--- ChessGame/King.cs
+++ ChessGame/King.cs
@@ -15,51 +15,55 @@ namespace ChessGame
             this.Image = player == Player.Player_One ? MyAssets.W_KingImg : MyAssets.B_KingImg;
             InCheck = false;
         }
+        #region Public methods
         public override List<Tile> GetValidMoves(Board board, Tile selectedTile)
         {
             List<Tile> validMoves = new List<Tile>();
 
-            validMoves.AddRange(OneInEachDirection(board, selectedTile));
-            IgnoreKing(validMoves);
+            validMoves.AddRange(OneInEachDirection(board, selectedTile, selectedTile.CurrentPiece.CurrentPlayer));
+            IgnoreKing(validMoves); // inherited
+            IgnoreOccupiedSpaces(validMoves); // ignore all spaces where a piece sits
 
             return validMoves;
         }
-
-        private List<Tile> OneInEachDirection(Board b, Tile t)
+        #endregion
+        #region Private methods
+        private List<Tile> OneInEachDirection(Board b, Tile t, Player player)
         {
             List<Tile> validMoves = new List<Tile>(); // list that will be returned
 
             int currentX = t.CoordinateX; // added for readability
             int currentY = t.CoordinateY;
-
             var currPiece = t.CurrentPiece.CurrentPlayer;
 
-            if (currPiece == Piece.Player.Player_One && GameManager.Turn == GameManager.PlayerTurn.p1)
-            {
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX - 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX - 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY, currentX - 1]); } catch { }
-            }
-            else if (t.CurrentPiece.CurrentPlayer == Piece.Player.Player_Two && GameManager.Turn == GameManager.PlayerTurn.p2)
-            {
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX - 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY - 1, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX + 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY + 1, currentX - 1]); } catch { }
-                try { validMoves.Add(b.Tiles[currentY, currentX - 1]); } catch { }
+            var turn = GameManager.Turn; // current turn
 
+            if ((int)player == (int)turn) 
+            {
+                try { validMoves.Add(b.Tiles[currentY - 1, currentX - 1]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY - 1, currentX]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY - 1, currentX + 1]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY, currentX + 1]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY + 1, currentX + 1]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY + 1, currentX]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY + 1, currentX - 1]); } catch { }
+                try { validMoves.Add(b.Tiles[currentY, currentX - 1]); } catch { }
             }
-                return validMoves;
+
+            return validMoves;
         }
-        
-
+        private void IgnoreOccupiedSpaces(List<Tile> validMoves)
+        {
+            int numofMoves = validMoves.Count;
+            for (int i = numofMoves; i >= 0; i--)
+                try
+                { // remove moves where there are friendly peices
+                    if (validMoves[i].CurrentPiece is Piece)
+                        validMoves.RemoveAt(i);
+                }
+                catch { }
+                
+        }
+        #endregion
     }
 }
