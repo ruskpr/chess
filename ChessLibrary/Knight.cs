8c522863e1bbc636a90b8a3be717552236b68d82
Rustu Koprulu <rustulp@gmail.com>
2022-11-08 12:03:31 -0700

    save

--- ChessLibrary/Knight.cs
+++ ChessLibrary/Knight.cs
@@ -13,31 +13,55 @@ namespace ChessLibrary
             this.Image = player == Player.Player_One ? Assets.W_KnightImg : Assets.B_KnightImg;
         #endregion
         #region Public Methods
-        public override void GetValidMoves(Board board, Tile selectedTile)
+        public override void GetValidMoves(Board board, Tile selTile)
         {
             CurrentValidMoves.Clear();
 
-            if (selectedTile.CurrentPiece.CurrentPlayer == Player.Player_One &&
-            GameManager.Turn == GameManager.PlayerTurn.p1)
-                CurrentValidMoves.AddRange(GetPlayerOneMoves(board, selectedTile));
+            //int currentTurn = (int)selTile.CurrentPiece.CurrentPlayer;
 
-            if (selectedTile.CurrentPiece.CurrentPlayer == Player.Player_Two &&
-            GameManager.Turn == GameManager.PlayerTurn.p2)
-                CurrentValidMoves.AddRange(GetPlayerTwoMoves(board, selectedTile));
+            if ((int)selTile.CurrentPiece.CurrentPlayer == (int)GameManager.Turn)
+                CurrentValidMoves.AddRange(GetMoves(board, selTile));
 
-            IgnoreKing(CurrentValidMoves);
+            //if ((int)selTile.CurrentPiece.CurrentPlayer == 1 && (int)GameManager.Turn == 1)
+            //    CurrentValidMoves.AddRange(GetPlayerOneMoves(board, selTile));
+
+            //if ((int)selTile.CurrentPiece.CurrentPlayer == 2 && (int)GameManager.Turn == 2)
+            //    CurrentValidMoves.AddRange(GetPlayerTwoMoves(board, selTile));
+
+            //IgnoreKing(CurrentValidMoves);
         }
         #endregion
         #region Private Methods
-        /// <summary>
-        /// 
-        /// 2 private methods, one for each player
-        /// 
-        /// </summary>
-        /// <param name="t"> refers to the selected parameter when public method is called in board class </param>
-        /// <param name="b"> refers to the board that the selected tile belongs to </param>
-        /// <returns></returns>
+        
         #region Player one movement
+        private List<Tile> GetMoves(Board b, Tile t)
+        {
+            List<Tile> validMoves = new List<Tile>(); // list that will be returned
+
+            //upper right
+            try { validMoves.Add(b.Tiles[t.CoordinateY - 2, t.CoordinateX + 1]); } catch { }
+            try { validMoves.Add(b.Tiles[t.CoordinateY - 1, t.CoordinateX + 2]); } catch { }
+
+            //upper left
+            try { validMoves.Add(b.Tiles[t.CoordinateY - 2, t.CoordinateX - 1]); } catch { }
+            try { validMoves.Add(b.Tiles[t.CoordinateY - 1, t.CoordinateX - 2]); } catch { }
+
+            //lower right
+            try { validMoves.Add(b.Tiles[t.CoordinateY + 2, t.CoordinateX + 1]); } catch { }
+            try { validMoves.Add(b.Tiles[t.CoordinateY + 1, t.CoordinateX + 2]); } catch { }
+
+            //lower left
+            try { validMoves.Add(b.Tiles[t.CoordinateY + 2, t.CoordinateX - 1]); } catch { }
+            try { validMoves.Add(b.Tiles[t.CoordinateY + 1, t.CoordinateX - 2]); } catch { }
+
+            // remove move if it contains a king or friendly player
+            foreach (Tile move in validMoves.ToList())
+                if (move.CurrentPiece is King || move.CurrentPiece != null &&
+                (int)move.CurrentPiece.CurrentPlayer == (int)GameManager.Turn)
+                    validMoves.Remove(move);
+
+            return validMoves;
+        }
         private List<Tile> GetPlayerOneMoves(Board b, Tile t)
         {
             List<Tile> validMoves = new List<Tile>(); // list that will be returned
