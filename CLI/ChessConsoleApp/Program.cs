925272515c7892160f5f0eed278cde53257a6d18
Russ Koprulu <rustulp@gmail.com>
2023-03-25 15:07:50 -0600

    updated

--- CLI/ChessConsoleApp/Program.cs
+++ CLI/ChessConsoleApp/Program.cs
@@ -9,7 +9,49 @@ internal class Program
 
         DrawBoard(board);
 
-        Console.ReadLine();
+        // take user input to make a move
+        while (true)
+        {
+            Console.WriteLine("Enter the piece you want to move");
+            var selectedPieceInput = Console.ReadLine();
+            int fromRow = Convert.ToInt32(selectedPieceInput[0].ToString());
+            int fromCol = Convert.ToInt32(selectedPieceInput[1].ToString());
+            var pieceToMove = board.GetPiece(fromRow, fromCol);
+            Console.WriteLine($"Selected: {pieceToMove.Color} {pieceToMove.Symbol}");
+            Console.WriteLine("Enter the tile you want to move to, type x to cancel");
+            string tileInput = Console.ReadLine();
+            if (tileInput == "x") continue;
+            int toRow = Convert.ToInt32(tileInput[0].ToString());
+            int toCol = Convert.ToInt32(tileInput[1].ToString());
+
+            var tileToMoveTo = board.GetTile(toRow, toCol);
+
+            Console.Clear();
+
+            if (pieceToMove == null)
+            {
+                Console.WriteLine("Invalid piece");
+                continue;
+            }
+            if (tileToMoveTo == null)
+            {
+                Console.WriteLine("Invalid tile");
+                continue;
+            }
+
+            if (board.TryMakeMove(board.GetTile(pieceToMove), tileToMoveTo))
+            {
+                DrawBoard(board);
+                continue;
+            }
+            else
+            {
+                Console.WriteLine("Invalid move");
+            }
+
+            DrawBoard(board);
+
+        }
 
     }
 
@@ -18,19 +60,39 @@ internal class Program
         // draw the chess board
         for (int i = 0; i < board.Size; i++)
         {
+            Console.WriteLine("---------------------------------");
+
             for (int j = 0; j < board.Size; j++)
             {
+
                 var tile = board.Tiles[i, j];
                 if (tile.Piece != null)
                 {
-                    Console.Write(tile.Piece.Symbol);
+
+                    Console.Write("|");
+                    if (j == 7)
+                    {
+                        Console.Write($" {tile.Piece.Symbol} |");
+                        continue;
+                    }
+
+                    Console.Write($" {tile.Piece.Symbol} ");
                 }
                 else
                 {
-                    Console.Write(" ");
+                    if (j == 7)
+                    {
+                        Console.Write("|   |");
+                        continue;
+                    }
+                    Console.Write("|   ");
                 }
             }
+
             Console.WriteLine();
         }
+        Console.WriteLine("---------------------------------");
+
     }
+
 }
