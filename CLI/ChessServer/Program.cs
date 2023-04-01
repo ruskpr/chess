ef240e40923da5ffe9cac31121c533ab47d735a0
Rustu Koprulu <rustulp@gmail.com>
2023-04-01 15:57:27 -0600

    added

---
+++ CLI/ChessServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using Chess.Core.TCP;
+
+namespace ChessServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server(30000);
+            server.OnPacketRecieved += Server_OnPacketRecieved;
+
+            Task.Run(() => server.Start()); 
+            
+            Console.ReadLine();
+        }
+
+        private static void Server_OnPacketRecieved(Packet packet)
+        {
+            Console.WriteLine(packet.Payload);
+        }
+    }
+}
--- Chess.Core/TCP/Server.cs
+++ Chess.Core/TCP/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,70 @@ namespace Chess.Core.TCP
 {
     public class Server
     {
+        #region delegates / events
+
+        public delegate void PacketRecieved(Packet packet);
+        public event PacketRecieved OnPacketRecieved;
+
+        #endregion
+
+        #region get only properties
+
+        public bool IsRunning { get => _running; }
+        public int Port { get => _port; }
+        public List<TcpClient> Clients { get => _clients; }
+
+        #endregion
+
+        #region fields
+
         TcpListener _listener;
+        private int _port;
+        private bool _running;
+        private List<TcpClient> _clients = new List<TcpClient>();
 
         Board _board;
         Player? _playerWhite;
         Player? _playerBlack;
 
-        public Server()
-        {
+        #endregion
 
+        public Server(int port)
+        {
+            _port = port;
+            _listener = new TcpListener(IPAddress.Any, port);
 
         }
 
-        public void StartServer()
+        public async Task Start()
         {
-            throw new NotImplementedException();
+            _listener.Start();
+            _running = true;
+
+            while (_running)
+            {
+                TcpClient client = await _listener.AcceptTcpClientAsync();
+
+                _clients.Add(client);
+
+                Task.Run(() => HandleClient(client));
+            }
         }
-        
+
+        public void Stop()
+        {
+            _running = false;
+            _listener.Stop();
+        }   
+
+        public async Task HandleClient(TcpClient client)
+        {
+            if (_listener.Pending())
+            {
+                client = _listener.AcceptTcpClient();
+                _clients.Add(client);
+            }
+        }
+
     }
 }
