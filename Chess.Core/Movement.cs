2d7ce3218aee59de77d2c86891597fe167340f28
Rustu Koprulu <rustulp@gmail.com>
2023-04-10 01:32:53 -0600

    ignore same color and avoid jumping over pieces

--- Chess.Core/Movement.cs
+++ Chess.Core/Movement.cs
@@ -1,15 +1,21 @@
-﻿namespace Chess.Core
+﻿using Chess.Core.Pieces;
+
+namespace Chess.Core
 {
     internal class Movement
     {
-        private static bool IsValid(Board board, BoardLocation current, int deltaRow, int deltaCol, out BoardLocation location)
+        private static bool IsValidAndInBounds(Board board, IPiece currPiece, int deltaRow, int deltaCol, out BoardLocation location)
         {
             location = null;
-            var newRow = current.Row + deltaRow;
+            var newRow = currPiece.CurrentLocation.Row + deltaRow;
             if ((newRow < 0) || (newRow > board.Size)) return false;
 
-            var newCol = current.Column + deltaCol;
+            var newCol = currPiece.CurrentLocation.Column + deltaCol;
             if ((newCol < 0) || (newCol > board.Size)) return false;
+
+
+            if (Collision(board, currPiece, newRow, newCol))
+                return false;
 
             location = new BoardLocation(newRow, newCol);
             return true;
@@ -24,15 +30,18 @@
 
             var ret = new List<Tile>();
 
-            foreach (var template in templates)
+            foreach (var template in templates) 
             {
                 for (var radius = 1; radius <= range; radius++)
                 {
                     var deltaX = radius * template[0];
                     var deltaY = radius * template[1];
 
-                    if (IsValid(board, piece.CurrentLocation, deltaX, deltaY, out BoardLocation newLocation))
-                    {
+                    if (IsValidAndInBounds(board, piece, deltaX, deltaY, out BoardLocation newLocation))
+                    {   
+                        if (IsSameColor(board, piece, newLocation))
+                            break;
+                        
                         ret.Add(new Tile(newLocation.Row, newLocation.Column));
                     }
                     else
@@ -40,9 +49,37 @@
                         break;
                     }
                 }
-            }
+            }           
 
             return ret;
+        }
+
+        private static bool IsSameColor(Board board, IPiece currPiece, BoardLocation newLoc)
+        {
+            Tile newTile = board.GetTile(newLoc.Row, newLoc.Column);
+
+            if (newTile == null) return false;
+            if (newTile.Piece == null) return false;
+
+            if (currPiece.Color != newTile.Piece.Color)
+                return false;
+            else
+                return true;
+        }
+
+        private static bool Collision(Board board, IPiece currPiece, int newRow, int newCol)
+        {
+            // knight is the only piece that can jump over other peices
+            // so it is impossible to collide with another piece
+            if (currPiece is Knight) return false;
+
+            Tile? newLoc = board.GetTile(newRow, newCol);
+            if (newLoc == null) return false;
+
+            // if there is a piece and the colors match
+            if (newLoc.Piece != null && newLoc.Piece.Color == currPiece.Color) return true;
+
+            return false;
         }
 
         internal static bool MoveIsValid(Board board, Tile from, Tile to)
