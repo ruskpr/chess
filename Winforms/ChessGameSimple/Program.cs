d85551ee74db3cf9bd5b992f10ad4aa5e195c789
Russ Koprulu <rustulp@gmail.com>
2023-04-15 02:21:40 -0600

    refactor

--- Winforms/ChessGameSimple/Program.cs
+++ Winforms/ChessGameSimple/Program.cs
@@ -15,10 +15,10 @@ namespace ChessGameSimple
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
-            AppContext appContext = new AppContext();
+            //AppContext appContext = new AppContext();
 
-            Application.Run(appContext);
-            //Application.Run(new FormLauncher());
+            //Application.Run(appContext);
+            Application.Run(new FormLauncher());
             //Application.Run(new FormPlayerVsPlayer());
         }
     }
